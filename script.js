e5316f02c551c8bd586633217d0c23c4af99a8fd
Josef <98840857+Botcoinista@users.noreply.github.com>
2022-10-17 17:24:21 +0200

    efter labb med Jesper

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-const firstname = document.getElementById ("firstName")
-const lastname = document.getElementById ("lastName")
-const email = document.getElementById ("email")
-const password = document.getElementById ("password")
-const repeatPassword = document.getElementById ("repeatPassword")
-const terms = document.getElementById ("terms")
-const form = document.getElementById ("validationForm")
-// const message = document.getElementById ("#message")
+const firstname = document.getElementById ("firstName");
+const lastname = document.getElementById ("lastName");
+const email = document.getElementById ("email");
+const password = document.getElementById ("password");
+const repeatPassword = document.getElementById ("repeatPassword");
+const terms = document.getElementById ("terms");
+const form = document.getElementById ("validationForm");
+// const messageEmail2 = document.getElementById ("messageEmail2")
 
+ 
 
-
-const validateText = (input) => {
+const validateTextFirstName = (input) => {
     if (input.value.trim() === "") {
-    // message.innerHTML = `Värdet ${input.value} är för lågt.`;
-   console.log("Du kan inte lämna fältet tomt!")
-   return false
-   error (input)
+         message.innerHTML = "First Name kan inte lämnas tomt.";
+         console.log("First Name kan inte lämnas tomt.");
+        
     }
-    else if (input.value.length < 2) {
-        console.log("Du måste ange ett namn med minst 2 bokstäver")
+    if (input.value.length < 2) { 
+        message2.innerHTML = "First Name måste innehålla minst två tecken.";
+        console.log("First Name måste innehålla minst två tecken.");
+        return false
+    }
+    return true
+    
+}
+
+const validateTextLastName = (input) => {
+    if (input.value.trim() === "") {
+    message3.innerHTML = "Last Name kan inte lämnas tomt.";
+   console.log("Last Name kan inte lämnas tomt.");
+   
+
+    }
+    if (input.value.length < 2 ) {
+        message4.innerHTML = "Last Name måste innehålla minst två tecken.";
+        console.log("Last Name måste innehålla minst två tecken.");
         return false
     }
     return true
@@ -29,12 +45,13 @@ const validateEmail = (email) => {
     const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/
 
     if (email.value.trim() === ""){
-        console.log("din mailadress kan inte vara tom")
+        messageEmail.innerHTML = "Du måste skriva in en email address!";
+        console.log("Du måste skriva in en email address!");
         return false;
-    // return error (email)
     }
-    else if (!regEx.test (email.value)) {
-        console.log("du m¨ste ha en giltig email")
+    else if (!regEx.test(email.value)) {
+        messageEmail2.innerHTML = "Du har inte inkluderat alla de tecken som krävs för en giltig email address";
+        console.log("Du har inte inkluderat alla de tecken som krävs för en giltig email address");
         return false;
     }
     else {
@@ -50,11 +67,13 @@ const validatePassword = (input) => {
     const regExPassword = /^\d$/
   
      if(input.value.trim() === ""){
-        console.log("Ditt lösenord kan inte vara tomt")
+        console.log("Ditt lösenord kan inte vara tomt");
+        messagePassword.innerHTML = ("Ditt lösenord kan inte vara tomt");
         return false;
      } 
      else if (input.value.length < 6) {
-        console.log("Ditt lösenord måste innehålla minst 6 tecken!")
+        console.log("Ditt lösenord måste innehålla minst 6 tecken!");
+        messagePassword2.innerHTML = ("Ditt lösenord måste innehålla minst 6 tecken!");
         return false;
      }
     //  else if (!regExPassword.test (password.value)) {
@@ -62,7 +81,8 @@ const validatePassword = (input) => {
     //     return false;
     // }
      else if (password.value !== repeatPassword.value){
-        console.log("Dina lösenord måste stämma överens")
+        console.log("Dina lösenord måste stämma överens");
+        messagePassword3.innerHTML = ("Dina lösenord måste stämma överens");
         return false;
      }
      else{
@@ -75,60 +95,67 @@ const validatePassword = (input) => {
     
         if (!input.checked) {
             console.log("Du måste kryssa i boxen!")
-            //return error (input)
+            messageTerms.innerHTML = "Du måste kryssa i boxen!"
             return false
         }
         else {
-            //return success(input)
             return true
         }
     
     }
 
 
-const success = (input) => {
-    return true;
+const success = () => {
+    console.log("Du har skapat ett formulär!")
+    messageUser.innerHTML = ("Du har skapat ett formulär! ")
+    const user = {
+        firstname: firstname.value, 
+        lastname: lastname.value,
+        email: email.value,
+        password: password.value
+    }
+    console.log(user)
 }
-
-const error = (input) => {
-    return false;
-}
-
 
 
 form.addEventListener( "submit", (e) =>  {
     e.preventDefault ();
 
-   
-   
-    validateText (firstname)
-    validateText (lastname)
-    validateEmail (email)
-    validatePassword (password)
-    validateTerms (terms)
-
-    if (validateText (firstname) && 
-        validateText (lastname) && 
+    if (validateTextFirstName (firstname) && 
+        validateTextLastName (lastname) && 
         validateEmail (email) &&
         validatePassword (password) &&
         validateTerms (terms)) 
         {
-        let test = success ()
-        const user = {
-            firstname: firstname.value, 
-            lastname: lastname.value,
-            email: email.value,
-            password: password.value
-        }
-        console.log("Du har skapat ett formulär!")
-        
-        console.log(user)
-        }
-    
-   
-
-
-        
-        
-        
+            success()
+        }      
 })
+
+// let firstname2 = "Josef";
+// let lastname2 = "Reinhold";
+
+// let FULLNAME = `Jag heter ${firstname2} i förnamn och ${lastname2} i efternamn!`
+// console.log(FULLNAME)
+
+
+// function calc (num1, num2, op){
+//     switch (op) {
+//         case "+":
+//             return num1 + num2 
+         
+//         case "-":
+//             return num1 - num2    
+            
+//         case "/":
+//             return num1 / num2 
+    
+//         default:
+//             console.log("invalid op")
+//             break;
+//     }
+    
+// }
+
+// const summa = calc (2,2,"+")
+// console.log(summa)
+// console.log(calc (2,2,"+"))
